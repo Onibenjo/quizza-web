9a163b3b199c6a0be8c320b7d570f4fe39dd8a65
Benjamin Oni <onibenjo@gmail.com>
2022-01-05 18:28:50 +0100

    feat: scoreboard #2

--- src/components/ScoreBoard/index.tsx
+++ src/components/ScoreBoard/index.tsx
@@ -2,10 +2,10 @@ export const ScoreBoard = ({ score }) => {
   return (
     <div className="flex justify-around min-h-screen items-center px-4">
       <div className="bg-gray-100 rounded-xl max-w-[450px] py-8 px-6 mx-auto w-full">
-        <table className="w-full">
+        <table className="w-full text-xl">
           <tr>
-            <th className="text-left text-xl uppercase">Contestant</th>
-            <th className="text-center text-xl uppercase">Score</th>
+            <th className="text-left text-2xl uppercase">Contestant</th>
+            <th className="text-center text-2xl uppercase">Score</th>
           </tr>
           {Object.entries(score).map(([contestant, score]) => (
             <tr key={contestant}>
